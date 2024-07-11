f9ffa3c749adfda5ccf14bc3eff2ae971b3e9160
yamdang <youm4000@naver.com>
2024-07-11 11:55:04 +0900

    feat: 리스트 페이지 내 크레딧 틀 완성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Landing from '@pages/LandingPage/Landing.jsx';
 import MyPage from '@pages/MyPage/MyPage.jsx';
+import MyCredit from '@pages/ListPage/MyCredit';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Landing />} />
         <Route path="/mypage" element={<MyPage />} />
+        <Route path="/list" element={<MyCredit />} />
         {/* <Route path="/list" element={<ListPage />} /> */}
       </Routes>
     </BrowserRouter>
