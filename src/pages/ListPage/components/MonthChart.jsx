0d7ce1d79a8a2022fab55fd4c1739daf66aa5da4
yamdang <youm4000@naver.com>
2024-07-19 19:37:55 +0900

    feat: 리스트 페이지 이달의 차트 api 연동&기능 구현

--- src/pages/ListPage/components/MonthChart.jsx
+++ src/pages/ListPage/components/MonthChart.jsx
@@ -1,33 +1,105 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
-import Button from '@components/Button';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import ChartImage from '@assets/svg/ic_chart.svg';
+import { getCharts } from '@apis/idolApi';
+import LoadingSpinner from '@components/LoadingSpinner';
+// component
+import Button from '@components/Button';
+import ChartButtonImage from '@assets/svg/ic_chart.svg';
 import MonthChartTab from './MonthChartTab';
-import GirlChartList from './GirlChartList';
-import BoyChartList from './BoyChartList';
+import MonthChartList from './MonthChartList';
 
 export default function MonthChart({ openModal }) {
-  const [tabOn, setTabOn] = useState({
-    girlTab: true,
-    boyTab: false,
-  });
+  const FEMALE = 'female';
+  const MALE = 'male';
+  const PAGE_SIZE = 10;
+  const [genderTab, setGenderTab] = useState(FEMALE);
+  const [chartPageSize, setChartPageSize] = useState(PAGE_SIZE);
+  const [nextCursorValue, setNextCursorValue] = useState();
+  const [moreButtonDisabled, setMoreButtonDisabled] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleClickMoreButton = () => {
+    setChartPageSize(chartPageSize + 10);
+  };
+
+  const handleClickShortButton = () => {
+    setChartPageSize(10);
+  };
+
+  useEffect(() => {
+    const handleLoad = async () => {
+      try {
+        const { nextCursor } = await getCharts({
+          gender: genderTab,
+          pageSize: chartPageSize,
+          cursor: 0,
+        });
+        setNextCursorValue(nextCursor);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    handleLoad();
+  }, [chartPageSize]);
+
+  useEffect(() => {
+    if (nextCursorValue === null) {
+      setMoreButtonDisabled(true);
+    } else {
+      setMoreButtonDisabled(false);
+    }
+  }, [nextCursorValue, chartPageSize]);
 
   return (
     <MyCreditWrap>
+      <LoadingSpinner
+        isLoading={isLoading}
+        color="var(--brand-coral)"
+        size={20}
+        minLoadTime={1000}
+      />
       <div>
         <ListPageSubTitle>
           <h2>이달의 차트</h2>
-          <Button onClick={() => openModal('VoteModal', tabOn)}>
-            <img src={ChartImage} alt="차트 이미지" />
+          <Button onClick={() => openModal('VoteModal', genderTab)}>
+            <img src={ChartButtonImage} alt="차트 이미지" />
             차트 투표하기
           </Button>
         </ListPageSubTitle>
       </div>
-      <MonthChartTab tabOn={tabOn} setTabOn={setTabOn} />
-      {tabOn.girlTab ? <GirlChartList /> : <BoyChartList />}
+      <MonthChartTab
+        genderTab={genderTab}
+        setGenderTab={setGenderTab}
+        setChartPageSize={setChartPageSize}
+      />
+      <MonthChartList
+        idolGender={genderTab === 'female' ? FEMALE : MALE}
+        chartPageSize={chartPageSize}
+      />
       <ChartMoreButton>
-        <button type="button">더보기</button>
+        <button
+          type="button"
+          onClick={() => {
+            handleClickMoreButton();
+          }}
+          className={moreButtonDisabled ? 'none' : ''}
+        >
+          더보기
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            handleClickShortButton();
+          }}
+          className={moreButtonDisabled ? '' : 'none'}
+        >
+          접기
+        </button>
       </ChartMoreButton>
     </MyCreditWrap>
   );
@@ -102,5 +174,9 @@ const ChartMoreButton = styled.div`
     border-radius: 3px;
     color: var(--white);
     background-color: var(--light-black);
+
+    &.none {
+      display: none;
+    }
   }
 `;
