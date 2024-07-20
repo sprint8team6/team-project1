d19bf90eaca68ec51123d5a461c6054638836551
yamdang <youm4000@naver.com>
2024-07-20 14:09:21 +0900

    style: 페이지 왼쪽 상단 배경 이미지 추가

--- src/pages/ListPage/ListPage.jsx
+++ src/pages/ListPage/ListPage.jsx
@@ -1,5 +1,6 @@
 import { ModalProvider, useModalContext } from '@contexts/useModalContext';
 import { CreditProvider } from '@contexts/useCreditContext';
+import styled from 'styled-components';
 // Modals
 import ChargeModal from '@components/Modal/ChargeModal';
 import PopupModal from '@components/Modal/PopupModal';
@@ -7,6 +8,7 @@ import DonationModal from '@components/Modal/DonationModal';
 import VoteModal from '@components/Modal/VoteModal';
 // Components
 import Header from '@components/Header';
+import LeftTopGradient from '@assets/svg/Image_top.svg';
 import MyCredit from './components/MyCredit';
 import TributeSupport from './components/TributeSupport';
 import MonthChart from './components/MonthChart';
@@ -42,6 +44,7 @@ function ListPageContent() {
         />
       )}
       <Header />
+      <LeftTopGradientDesign />
       <MyCredit openModal={openModal} />
       <TributeSupport />
       <MonthChart openModal={openModal} />
@@ -58,3 +61,17 @@ export default function ListPage() {
     </CreditProvider>
   );
 }
+
+export const LeftTopGradientDesign = styled.div`
+  display: block;
+  position: absolute;
+  width: 199px;
+  height: 273px;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  background-image: url(${LeftTopGradient});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: top;
+`;
