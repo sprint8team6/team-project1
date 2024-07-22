cc8d830dba18c28c08e540acf4cc3ece38fb7789
nulllife2510 <122213489+nulllife2510@users.noreply.github.com>
2024-07-22 11:08:43 +0900

    refactor: 불필요한 파일 및 assets 삭제

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import GlobalStyle from '@styles/GlobalStyle';
-import '@styles/global.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
